aefc29aee72938039ccd52068838a94cb8fb0738
Rodrigo Thauby <rthauby@gmail.com>
2019-07-09 14:14:47 -0400

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import NavBar from "./components/Navbar";
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+
 function App() {
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
