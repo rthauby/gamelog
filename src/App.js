df1ab0fc6f18db4a89aebfd4b317a7168cc26be9
Rodrigo Thauby <rthauby@gmail.com>
2019-07-09 14:09:55 -0400

    shit it stable

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// src/index.js
 
-import config from './config'
-// import SearchBar from './components/SearchBar'
-import GameList from './components/GameList'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "./react-auth0-wrapper";
+import config from "./auth_config.json";
 
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-
-window.gl = config
-
-function App() {
-  return (
-    <div>
-      caca
-    </div>
-
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
   );
-}
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+>
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
