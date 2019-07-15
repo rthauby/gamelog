c275e8ecfe2a989948e7d63b4b81154cc2d8f294
Rodrigo Thauby <rthauby@gmail.com>
2019-07-15 16:55:17 -0400

    refactoring search a bit

--- frontend/src/components/SearchList.jsx
+++ frontend/src/components/SearchList.jsx
@@ -4,9 +4,10 @@ import Grid from '@material-ui/core/Grid'
 import Game from './Game'
 
 export default function SearchList(props) {
+  const [results, setResults] = React.useState([])
   return (
     <Grid container spacing={10} style={{padding: '24px'}}>
-      {props.results.map((game) => {
+      {results.map((game) => {
         return <Grid key={game.id} item xs={12} sm={6} lg={4} xl={3}>
           <Game game={game}/>
         </Grid>
