8f928da57cf5d1ee1423032759e886156748da42
Rodrigo Thauby <rthauby@gmail.com>
2019-07-16 11:08:36 -0400

    searchlist back to functional component

--- frontend/src/components/SearchList.jsx
+++ frontend/src/components/SearchList.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid'
 import axios from 'axios'
 import Typography from '@material-ui/core/Typography'
 
 import config from '../config'
-
 import Game from './Game'
 
 async function getResults(query, callback) {
@@ -21,53 +20,26 @@ async function getResults(query, callback) {
     })
 }
 
-class SearchList extends React.Component {
+export default function SearchList(props) {
+  const query = props.match.params.query
+  const [results, setResults] = useState([])
 
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    getResults(query, setResults)
+  },[query])
 
-    this.state = {
-      results : []
-    }
-
-    this.setState.bind(this)
-  }
-
-  componentWillMount() {
-    this.props.history.listen(() => {
-      this.getAllResults()
-    });
-  }
-
-  componentDidMount() {
-    this.getAllResults()
-  }
-
-  getAllResults() {
-    const query = this.props.match.params.query
-    getResults(query, (results) => {
-      this.setState({
-        results
-      })
-    })
-  }
-
-  render() {
-    return (
-      <div style={{marginTop: '100px', padding: '0 24px'}}>
-        <Typography component="h3">
-          Search Results
-        </Typography>
-        <Grid container spacing={10}>
-          {this.state.results ? this.state.results.map((game) => {
-            return <Grid key={game.id} item xs={12} sm={6} lg={4} xl={3}>
-              <Game game={game}/>
-            </Grid>
-          }) : null}
-        </Grid>
-      </div>
-    )
-  }
+  return (
+    <div style={{marginTop: '100px', padding: '0 24px'}}>
+      <Typography component="h3">
+        Search Results
+      </Typography>
+      <Grid container spacing={10}>
+        {results ? results.map((game) => {
+          return <Grid key={game.id} item xs={12} sm={6} lg={4} xl={3}>
+            <Game game={game}/>
+          </Grid>
+        }) : null}
+      </Grid>
+    </div>
+  )
 }
-
-export default SearchList
